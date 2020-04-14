8a025860054a762be5ce0f660cdce68e8609be46
Marcus Oliveira <marcus.oliveira@m4u.com.br>
2020-04-14 20:48:30 -0300

    Implementa rotas de api para armazenamento de repositórios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,84 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  const { title } = request.query;
+
+  const results = title ? repositories.filter(repo => repo.title.includes(title)) : repositories;
+  
+  return response.status(200).json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const { title, url, techs } = request.body;
+  
+  const repository = { 
+    id:uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes:0 
+  }
+  
+  repositories.push(repository);
+  
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+  
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+  
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Invalid id'});
+  }
+  
+  const repository = {
+    id, 
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+  
+  repositories[repositoryIndex] = repository;
+  
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  
+  if (repoIndex < 0) {
+    return response.status(400).json({error: 'repository not found'});
+  }
+  
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send()
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  
+  if (repoIndex < 0) {
+    return response.status(400).json({error: 'Repository does not exist'});
+  }
+  
+  const repository = repositories[repoIndex];
+  
+  repository.likes++;
+  
+  return response.status(200).json(repository);
 });
 
 module.exports = app;
